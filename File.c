2e0316a49d6a4eb9f56ab58f59db4df32331f735
Justine <justine.pironnet@student.uclouvain.be>
2019-04-30 18:41:03 +0200

    Fin du read et modif semaphore

--- File.c
+++ File.c
@@ -10,13 +10,13 @@ void cracker(File.bin){
 	u_int8_t buffer1[3];//taille M+2 avec M qui vaut le nombre de type de fichier (1, 2 ou 3)
 	char* buffer2[5];//taille N+2 avec N le nombre de threads		
 	void lire(){//producteur qui lit dans les fichiers
-		sem_t *sem1=(sem_t *) malloc(sizeof(sem_t));
-		int s=sem_init(&sem1, 0, 3);// deuxieme parametre 0 je pense et c est &sem1 et pas sem1 car on a besoin de l adresse pointee par sem1
+		sem_t sem1;
+		int s=sem_init(&sem1, 0, 3);// initialise la semaphore
 		if(s==-1){
 			perror("create semaphore");
 		}
-		pthread_mutex_t *mut1=(pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
-		if (pthread_mutex_init(mut1, NULL)!=0){//si erreur c est different de 0 
+		pthread_mutex_t mut1;
+		if (pthread_mutex_init(&mut1, NULL)!=0){ //si erreur different de 0 
 			perror("mutex init"); 
 		}
 		
@@ -24,20 +24,25 @@ void cracker(File.bin){
 		if(fd1==-1){
 			perror("open file");
 		}
-		int r=read(fd1, buffer1, sizeof(u_int8_t));//lit le fichier
-		if(r==-1){
-			close(fd1); //on ferme le fd qui a ete ouvert
+		u_int8_t *rbuf1 = (u_int8_t *)malloc(sizeof(u_int8_t)); //buffer pour read
+		int r=read(fd1, rbuf1, sizeof(u_int8_t)); //lit le fichier
+		if(r==-1){ //erreur
+			close(fd1); //on ferme le fd qui a ete ouvert en cas d erreur de read
 			perror("read file");
 		}
-		while(r>=0){//Tant qu'il y a a lire
-			r=read(fd1, buffer1, sizeof(u_int8_t));//lit le fichier
-			if(r==0){ //comprends pas bien
+		while(r>0){//Tant qu'il y a a lire
+			r=read(fd1, rbuf1, sizeof(u_int8_t));//lit le fichier
+			if(r==-1){
+				close(fd1); //on ferme le fd qui a ete ouvert en cas d erreur de read
+				perror("read file");
+			}	
+			if(r!=0){ //tant qu il reste quelque hose a lire
 				u_int8_t hash;//cree la variable hash qui stockera ce qui est lu
-				hash=fd1; //donner la valeur a la variable hash
+				hash=*rbuf1; //donner la valeur a la variable hash
 				sem_wait(&sem1); //peut etre regarder en cas d erreur
-				pthread_mutex_lock(&mut1); // j ai mis l adresse de mut1 pas mut1
+				pthread_mutex_lock(&mut1); //adresse de mut1 pas mut1
 				buffer1[0]=hash;//inserer le hash dans le buffer, je ne sais pas comment le faire entre ou il y a une place libre donc je le mets en position 0 mais a changer
-				pthread_mutex_unlock(mut1);
+				pthread_mutex_unlock(&mut1);//adresse et pas mut1
 				sem_post(&sem1); //meme chose en cas d erreur
 			}
 		}
--- test.c
+++ test.c
@@ -10,99 +10,116 @@
 #include "sha256.c"
 #include "reverse.h"
 #include "reverse.c"
-	u_int8_t buffer1[3];//taille M+2 avec M qui vaut le nombre de type de fichier (1, 2 ou 3)
-	char* buffer2[5];//taille N+2 avec N le nombre de threads
-	int size1=3;//taille de buffer1, sert plus tard
-	int size2=5;//taille de buffer2, sert plus tard
-	int debut1=0;//debut du buffer1 a partir de quoi le buffer est vide
-	int fin1=0;//fin de la zone vide de buffer1
-	int debut2=0;//idem que debut1 mais pour buffer2
-	int fin2=0;//idem que fin1 mais pour buffer2
-	void lire(){//producteur qui lit dans les fichiers
-		int 
-		sem_t sem1;
-		int s=sem_init(&sem1, 0, 3);// deuxieme parametre 0 je pense et c est &sem1 et pas sem1 car on a besoin de l adresse pointee par sem1
-		if(s==-1){
-			perror("create semaphore");
-		}
-		pthread_mutex_t mut1;
-		if (pthread_mutex_init(&mut1, NULL)!=0){//si erreur c est different de 0 
-			perror("mutex init"); 
-		}
+u_int8_t buffer1[3];//taille M+2 avec M qui vaut le nombre de type de fichier (1, 2 ou 3)
+char* buffer2[5];//taille N+2 avec N le nombre de threads
+int size1=3;//taille de buffer1, sert plus tard
+int size2=5;//taille de buffer2, sert plus tard
+int debut1=0;//debut du buffer1 a partir de quoi le buffer est vide
+int fin1=0;//fin de la zone vide de buffer1
+int debut2=0;//idem que debut1 mais pour buffer2
+int fin2=0;//idem que fin1 mais pour buffer2
+
+void lire(){//producteur qui lit dans les fichiers
+	sem_t sem1;
+	int s=sem_init(&sem1, 0, 3);// initilise la semaphore1
+	if(s==-1) //si erreur
+		perror("create semaphore");
+
+	pthread_mutex_t mut1;
+	int err=pthread_mutex_init(&mut1, NULL);//initialise le mutex1
+	if (err!=0) //si erreur
+		perror("mutex init"); 
+	
+	int fd1=open("test-input/01_4c_1k.bin", O_RDONLY);//ouverture du fichier
+	if(fd1==-1) //si erreur
+		perror("open file");
 		
-		int fd1=open("test-input/01_4c_1k.bin", O_RDONLY);//ouverture du fichier
-		if(fd1==-1){
-			perror("open file");
-		}
-		u_int8_t *rbuf1 = (u_int8_t *)malloc(sizeof(u_int8_t)); //buffer pour read
-		int r=read(fd1, rbuf1, sizeof(u_int8_t));//lit le fichier
-		if(r==-1){ //erreur
-			close(fd1); //on ferme le fd qui a ete ouvert en cas d erreur de read
-			perror("read file");
-		}
-		while(r>0){//Tant qu'il y a a lire
-			r=read(fd1, rbuf1, sizeof(u_int8_t));//lit le fichier
-			if(r==-1){
-				close(fd1); //on ferme le fd qui a ete ouvert en cas d erreur de read
-				perror("read file");
-			}	
-			if(r!=0){ //tant qu il reste quelque hose a lire
-				u_int8_t hash;//cree la variable hash qui stockera ce qui est lu
-				hash=*rbuf1; //donner la valeur a la variable hash
-				sem_wait(&sem1); //peut etre regarder en cas d erreur
-				pthread_mutex_lock(&mut1); //adresse de mut1 pas mut1
-				buffer1[debut1]=hash;//inserer le hash dans le buffer a la premiere case libre
-				if(debut1==(size1-1)){//si debut1 est a la derniere case
-					debut1=0;//revient au debut
-				}
-				else{
-					debut1++;//sinon debut1 augmente de 1;
-				}
-				pthread_mutex_unlock(&mut1);//adresse et pas mut1
-				sem_post(&sem1); //meme chose en cas d erreur
-			}
-		}
+	u_int8_t *rbuf1 = (u_int8_t *)malloc(sizeof(u_int8_t)); // cree le buffer pour read
+	if(read(fd1, rbuf1, sizeof(u_int8_t))==-1){ //si erreur
+		close(fd1); //on ferme le fd qui a ete ouvert en cas d erreur de read
+		perror("read file");
 	}
-//consommateur 1 et producteur 2 
-	void decrypteur(){//threads de calculs
-	u_int8_t mdp;
-	sem_t sem1;//recree sem1 pour compilation ici mais dans cracker ce sera le meme que au dessus
-	int s=sem_init(&sem1, 0, 3);
-	if(s==-1){//en cas d erreur
-		perror("create semaphore1");
-	}
-	sem_t sem2;
-	int s2=sem_init(&sem2, 0, 5);
-	if(s2==-1){//en cas d erreur
-		perror("create semaphore2");
-	}
-	pthread_mutex_t mut1;// je recree mut1 ici mais il faudra le faire une seule fois dans cracker au debut
-	pthread_mutex_init(&mut1, NULL);// verifier si pas erreur ?
-	pthread_mutex_t mut2;//Pour le test je recree un mutex mais toujours le meme dans cracker je pense
-	pthread_mutex_init(&mut2, NULL);// verifier si pas erreur ?
-	while(1==1){//tant qu'il y a a decrypter dans buffer1, je ne sais pas comment faire ca donc j ecris time temporairement pour compiler
-		sem_wait(&sem1);//attente d'un slot rempli de buffer1 attention inverse
-		pthread_mutex_lock(&mut1);
-		mdp=buffer1[fin1];//prend la premiere valeur de buf1 apres l espace vide
-		if(fin1==(size1-1)){//si fin1 est a la derniere case
-			fin1=0;//revient au debut
-		}
-		else{
-			fin1++;//sinon fin1 augmente de 1;
-		}
-		pthread_mutex_unlock(&mut1);
-		sem_post(&sem1);//ce sera l inverse 
-		mdp=reversehash(mdp);//applique reversehash
-		sem_wait(&sem2);//attente d'un slot libre
-		pthread_mutex_lock(&mut2);
-		buffer2[debut2]=mdp;
-		if(debut2==(size2-1)){//si debut2 est a la derniere case
-			debut2=0;//revient au debut
-		}
-		else{
-			debut2++;//sinon debut2 augmente de 1;
-		}
-		pthread_mutex_unlock(&mut2);
-		sem_post(&sem2);
+
+	while(read(fd1, rbuf1, sizeof(u_int8_t))>0){//Tant qu'il y a a lire
+		u_int8_t hash;//cree la variable hash qui stockera ce qui est lu
+		hash=*rbuf1; //donner la valeur a la variable hash
+
+		sem_wait(&sem1); //peut etre regarder en cas d erreur
+
+		int err1 = pthread_mutex_lock(&mut1); //lock le mut1
+		if(err1!=0)//si erreur
+			perror("mutex lock read"); 
+
+		buffer1[debut1]=hash;//inserer le hash dans le buffer a la premiere case libre
+		if(debut1==(size1-1))//si debut1 est a la derniere case
+			debut1=0;//revient au debut
+		else
+			debut1++;//sinon debut1 augmente de 1;
+
+		err1 = pthread_mutex_unlock(&mut1);//unlock le mut1
+		if(err1!=0)//si erreur
+			perror("mutex unlock read");
+		sem_post(&sem1); //meme chose en cas d erreur
 	}
 }
+//consommateur 1 et producteur 2 
+void decrypteur(){//threads de calculs
+	u_int8_t mdp; //mdp de 8 bits
+	sem_t empty1; 
+	int e1=sem_init(&empty1, 0, 3);//cree semaphore vide qui compte 3 slots vides consommateur 1
+	if(e1==-1)//si erreur
+		perror("create empty1");
+		
+	sem_t full1;
+	int f1=sem_init(&full1, 0, 0); //cree semaphore vide qui compte 0 slot rempli consommateur 1
+	if(f1==-1)//si erreur
+		perror("create full1");
+	
+	sem_t empty2; 
+	int e2=sem_init(&empty2, 0, 3);//cree semaphore vide qui compte 5 slots vides producteur 2
+	if(e2==-1)//si erreur
+		perror("create empty2");
+		
+	sem_t full2;
+	int f2=sem_init(&full2, 0, 0); //cree semaphore vide qui compte 0 slot rempli producteur 2
+	if(f2==-1)//si erreur
+		perror("create full2");
+
+	pthread_mutex_t mut1;// je recree mut1 ici mais il faudra le faire une seule fois dans cracker au debut
+	int err1 = pthread_mutex_init(&mut1, NULL);
+	if(err1!=0)//si erreur
+		perror("init mutex1 decrypt");
+
+	pthread_mutex_t mut2;//Pour le test je recree un mutex mais toujours le meme dans cracker je pense
+	int err2 = pthread_mutex_init(&mut2, NULL);
+	if(err2!=0)//si erreur
+		perror("init mutex2 decrypt");
+
+	while(true){//tant qu il y a a decrypter dans buffer1 je ne sais pas comment faire ca donc j ecris time temporairement pour compiler
+		sem_wait(&empty1);//attente d un slot libre (3 max)
+		pthread_mutex_lock(&mut1); //lock le mut1
+		mdp=buffer1[fin1];//prend la premiere valeur de buf1 apres l espace vide
+
+		if(fin1==(size1-1))//si fin1 est a la derniere case
+			fin1=0;//revient au debut
+		else
+			fin1++;//sinon fin1 augmente de 1;
+
+		mdp=reversehash(mdp);//applique reversehash	
+		pthread_mutex_unlock(&mut1); //unlock mut1
+		sem_post(&full1); //1 slot rempli en plus (3 max)
+
+		sem_wait(&empty2);//attente d'un slot libre (5max)
+		pthread_mutex_lock(&mut2);
+		buffer2[debut2]=mdp;
+
+		if(debut2==(size2-1))//si debut2 est a la derniere case
+			debut2=0;//revient au debut
+		else
+			debut2++;//sinon debut2 augmente de 1;
+		
+		pthread_mutex_unlock(&mut2);
+		sem_post(&full2); //1 slot rempli en plus (5 max)
+	}
+}
+
