9cf8842c6f56579bbd7837c75302a9c0da0cc146
yoyomanhihi <thibaud.misonne@hotmail.com>
2019-05-02 11:01:52 +0200

    Condition while, reglage de warnings
    Condition des while ajoutees et corrections de petits problemes

--- test.c
+++ test.c
@@ -18,22 +18,24 @@ int debut1=0;//debut du buffer1 a partir de quoi le buffer est vide
 int fin1=0;//fin de la zone vide de buffer1
 int debut2=0;//idem que debut1 mais pour buffer2
 int fin2=0;//idem que fin1 mais pour buffer2
+bool lecture=true;//indique que lire est encore en cours
+bool decryptage=true;//indique que decrypteur est encore en cours
 
 void lire(){//producteur qui lit dans les fichiers
 	sem_t sem1;
 	int s=sem_init(&sem1, 0, 3);// initialise le semaphore1
-	if(s==-1) //si erreur
+	if(s==-1){ //si erreur
 		perror("create semaphore");
-
+		}
 	pthread_mutex_t mut1;
 	int err=pthread_mutex_init(&mut1, NULL);//initialise le mutex1
-	if (err!=0) //si erreur
+	if (err!=0){ //si erreur
 		perror("mutex init"); 
-	
+	}
 	int fd1=open("test-input/01_4c_1k.bin", O_RDONLY);//ouverture du fichier
-	if(fd1==-1) //si erreur
+	if(fd1==-1){ //si erreur
 		perror("open file");
-		
+	}
 	u_int8_t *rbuf1 = (u_int8_t *)malloc(sizeof(u_int8_t)); // cree le buffer pour read
 	if(read(fd1, rbuf1, sizeof(u_int8_t))==-1){ //si erreur
 		close(fd1); //on ferme le fd qui a ete ouvert en cas d erreur de read
@@ -47,25 +49,29 @@ void lire(){//producteur qui lit dans les fichiers
 		sem_wait(&sem1); //peut etre regarder en cas d erreur
 
 		int err1 = pthread_mutex_lock(&mut1); //lock le mut1
-		if(err1!=0)//si erreur
+		if(err1!=0){//si erreur
 			perror("mutex lock read"); 
-
+		}
 		buffer1[debut1]=hash;//inserer le hash dans le buffer a la premiere case libre
-		if(debut1==(size1-1))//si debut1 est a la derniere case
+		if(debut1==(size1-1)){//si debut1 est a la derniere case
 			debut1=0;//revient au debut
-		else
+		}
+		else{
 			debut1++;//sinon debut1 augmente de 1;
-
+		}
 		err1 = pthread_mutex_unlock(&mut1);//unlock le mut1
-		if(err1!=0)//si erreur
+		if(err1!=0){//si erreur
 			perror("mutex unlock read");
+		}
 		sem_post(&sem1); //meme chose en cas d erreur
 	}
+	lecture=false;
+	
 }//le read est ok normalement
 
 int count(char* mot){ //compter les voyelles ou les consonnes mais faudra regarder comment on gere ce cas
 	int len=strlen(mot); //variable qui comporte la taille du mot 
-	int count=0; //nombre de voyelle ou consonnz
+	int count=0; //nombre de voyelle ou consonne
 	if(true) {//pas le bon if mais pour dire que le critere est voyelle
 		for(int i=0; i<len; i++){
 			if(mot[i]=='a' || mot[i]=='e' || mot[i]=='i' || mot[i]=='o' || mot[i]=='u' || mot[i]=='y')
@@ -83,8 +89,8 @@ int count(char* mot){ //compter les voyelles ou les consonnes mais faudra regard
 
 //consommateur 1 et producteur 2 
 void decrypteur(){//threads de calculs
-	char* bufferInter; //buffer intermediaire utile a la fonction reversehash je beug jsp comment faire pour que le mot soit dans bufferinter
-	u_int8_t *mdp=0; 
+	char *bufferInter=(char *) malloc(sizeof(char)*17); //buffer intermediaire utile a la fonction reversehash je beug jsp comment faire pour que le mot soit dans bufferinter
+	u_int8_t *mdp=0;
 	sem_t empty1; 
 	int e1=sem_init(&empty1, 0, 3);//cree semaphore vide qui compte 3 slots vides consommateur 1
 	if(e1==-1)//si erreur
@@ -115,29 +121,34 @@ void decrypteur(){//threads de calculs
 	if(err2!=0)//si erreur
 		perror("init mutex2 decrypt");
 	
-	while(true){//tant qu il y a a decrypter dans buffer1 je ne sais pas comment faire ca donc j ecris time temporairement pour compiler
+	while((lecture==true)&&(debut1!=fin1)){//tant que le thread a encore a lire et que le buffer n est pas vide, juste a verifier que la condition debut1!=fin1 n accepte pas un buffer totalement rempli
 		sem_wait(&empty1);//attente d un slot libre (3 max)
 		pthread_mutex_lock(&mut1); //lock le mut1
 		*mdp=buffer1[fin1];//prend la premiere valeur de buf1 apres l espace vide
-		if(fin1==(size1-1))//si fin1 est a la derniere case
+		if(fin1==(size1-1)){//si fin1 est a la derniere case
 			fin1=0;//revient au debut
-		else
+		}
+		else{
 			fin1++;//sinon fin1 augmente de 1;
+		}
 		pthread_mutex_unlock(&mut1); //unlock mut1
 		sem_post(&full1); //1 slot rempli en plus (3 max)
 		sem_wait(&empty2);//attente d'un slot libre (5 max)
 		pthread_mutex_lock(&mut2);
 		if (reversehash(mdp, bufferInter, 16)==true){//si reversehash a trouve un inverse il le stocke dans bufferInter mais??
-			buffer2[debut2]=*bufferInter;
-			if(debut2==(size2-1))//si debut2 est a la derniere case
+			if(debut2==(size2-1)){//si debut2 est a la derniere case
 				debut2=0;//revient au debut
-			else
+			}
+			else{
 				debut2++;//sinon debut2 augmente de 1;
+			}
 		}
 		pthread_mutex_unlock(&mut2);
 		sem_post(&full2); //1 slot rempli en plus (5 max)
 	}
+	decryptage=false;	
 }
+
 void ecrire(){
 	sem_t empty2; 
 	int e2=sem_init(&empty2, 0, 5);//cree semaphore vide qui compte 5 slots vides producteur 2
@@ -165,15 +176,16 @@ void ecrire(){
 	if(fd2==-1)
 		perror("open File write");
 	
-	while(true){//tant qu il y a a analyser dans buffer2
+	while((decryptage==true)&&(debut2!=fin2)){//tant que la fonction decrypteur n a pas tremine et que le buffer2 n est pas vide, verifier condition 2 que pas de faille
 		sem_wait(&full2);
 		pthread_mutex_lock(&mut2);
-		(*candidat)=buffer2[fin2];
-		if(fin2==(size2-1))//si fin2 est a la derniere case
+		(*candidat)=buffer2[fin2];// pas compris
+		if(fin2==(size2-1)){//si fin2 est a la derniere case
 			fin2=0;//revient au debut
-		else
+		}
+		else{
 			fin2++;//sinon fin2 augmente de 1;
-		
+		}
 		pthread_mutex_unlock(&mut2);
 		sem_post(&empty2);//pour moi le sem_post doit pas etre la truc bizarre avec le candidat
 		int test=count(candidat);//voir si c est mieux une fonction pour cons ET voy ou voir si plus opti de faire deux fonctions en fonction du critere
@@ -182,9 +194,9 @@ void ecrire(){
 			char* wbuff1=malloc (sizeof(char)*(strlen(candidat)+1));
 			freopen("File", "w", "File");//ferme et reouvre le fichier en le vidant w pour le mode ecriture
 			int b=write(fd2, wbuff1, sizeof(char)*(strlen(candidat)+1));//candidat va etre ecrit dans le fichier File
-			if(b==-1)
+			if(b==-1){
 				perror("write error");
-			
+			}
 			max=test; //max prend donc la valeur de test
 			pthread_mutex_unlock(&mut3);
 		}
@@ -192,20 +204,10 @@ void ecrire(){
 			pthread_mutex_lock(&mut3);
 			char* wbuff1=malloc (sizeof(char)*(strlen(candidat)+1));
 			int b=write(fd2, wbuff1, sizeof(char)*(strlen(candidat)+1));
-			if(b==-1)
+			if(b==-1){
 				perror("write error");
-			
+			}
 			pthread_mutex_unlock(&mut3);
 		}
 	}	
 }
-
-
-
-
-
-
-
-
-
-
