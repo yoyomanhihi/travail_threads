4edcc64dfabdbd2e83288a9fe0174075baf9a0e8
Justine <justine.pironnet@student.uclouvain.be>
2019-05-06 16:59:15 +0200

    qq modif

--- test.c
+++ test.c
@@ -20,20 +20,18 @@ int debut1;//debut du buffer1 a partir de quoi le buffer est vide
 int fin1;//fin de la zone vide de buffer1
 int debut2=0;//idem que debut1 mais pour buffer2
 int fin2=0;//idem que fin1 mais pour buffer2
-//bool lecture=true;//indique que lire est encore en cours
-//bool decryptage=true;//indique que decrypteur est encore en cours
-pthread_mutex_t mut1;
 sem_t empty1; 
 sem_t empty2;
 sem_t full1;
 sem_t full2;
+pthread_mutex_t mut1;
 pthread_mutex_t mut2;
 pthread_mutex_t mut3;//mutex servant a ecrire dans fichier de sortie
-typedef struct node{
+typedef struct node{//represente un noeud 
 	struct node *next;
 	char* candid;
 } node_t;
-typedef struct list{
+typedef struct list{//represente la liste qui sert a stocker les candidats dans ecrire
 	struct node *first;
 	int sizelist;
 } list_t;
@@ -55,18 +53,18 @@ int count(char* mot){ //compter les voyelles ou les consonnes mais faudra regard
 	}
 	return count;
 }
-
-void *lire(void *fd){//producteur qui lit dans les fichiers
+//producteur 1
+void *lire(void *fd){//producteur 1 qui lit dans les fichiers
 	int fd1=*(int *)fd;
 	buffer1=(uint8_t **) malloc(3*sizeof(uint8_t *));
 	debut1=0;
 	u_int8_t *rbuf1 = (u_int8_t *)malloc(sizeof(u_int8_t)*32); // cree le buffer pour read
 	while(read(fd1, rbuf1, sizeof(u_int8_t)*32)>0){//Tant qu'il y a a lire
 		u_int8_t *rbuf = (u_int8_t *)malloc(sizeof(u_int8_t)*32); // cree le buffer pour read7
-		for(int i=0; i<32; i++){//copie de rbuf1 dans rbuf ! 
+		for(int i=0; i<32; i++){//copie de rbuf1 dans rbuf 
 			rbuf[i]=rbuf1[i];
 		}
-		sem_wait(&empty1); //peut etre regarder en cas d erreur
+		sem_wait(&empty1); 
 		int err1 = pthread_mutex_lock(&mut1); //lock le mut1
 		if(err1!=0){//si erreur
 			perror("mutex lock read"); 
@@ -80,9 +78,9 @@ void *lire(void *fd){//producteur qui lit dans les fichiers
 		if(err1!=0){//si erreur
 			perror("mutex unlock read");
 		}
-		sem_post(&full1); //meme chose en cas d erreur
+		sem_post(&full1); 
 	}
-	//lecture=false;//peut etre lock pour eviter probleme a la fin
+	//lecture=false;
 	pthread_exit(NULL);	
 }
 
@@ -92,8 +90,7 @@ void *decrypteur(){//threads de calculs
 	char *bufferInter=(char *) malloc(sizeof(char)*17); //buffer intermediaire utile a la fonction reversehash
 	fin1=0;
 	u_int8_t *mdp;
-	int j = 0; //pour la boucle
-	while((debut1!=fin1)||j<taille_fichier){//tant que le thread a encore a lire et que le buffer n est pas vide, juste a verifier que la condition debut1!=fin1 n accepte pas un buffer totalement rempli
+	while((debut1!=fin1)||taille_fichier>0){//tant qu on a pas decrypte tous les elements du fichier
 		sem_wait(&full1);//attente d un slot rempli
 		pthread_mutex_lock(&mut1); //lock le mut1
 		mdp=buffer1[fin1];//prend la premiere valeur de buf1 apres l espace vide
@@ -115,7 +112,7 @@ void *decrypteur(){//threads de calculs
 			else{
 				debut2++;//sinon debut2 augmente de 1;
 			}
-			j++;
+			taille_fichier--;;
 			pthread_mutex_unlock(&mut2);
 			sem_post(&full2); //1 slot rempli en plus (5 max)
 		}
@@ -128,8 +125,7 @@ void *ecrire(){
 	list_t *list=(list_t *) malloc(sizeof(list_t));
 	int max=-1;//maximum de voyelles ou consonnes lu
 	char* candidat=NULL;//variabe servant a stocker la valeur lue dans buffer2 initialisee a null au depart
-	int j =0;
-	while(j<taille_fichier||debut2!=fin2){//tant que j n'a pas atteint la taille du fichier ou que debut2 est different de fin2
+	while(taille_fichier>0||debut2!=fin2){//tant que j n'a pas atteint la taille du fichier ou que debut2 est different de fin2
 		sem_wait(&full2);
 		pthread_mutex_lock(&mut2);
 		candidat=buffer2[fin2];// verifier que pas de probleme
@@ -144,14 +140,14 @@ void *ecrire(){
 		sem_post(&empty2);
 		if(test>max){
 			pthread_mutex_lock(&mut3);
-			node_t *n=(node_t *) malloc(sizeof(node_t));
+			node_t *n=(node_t *) malloc(sizeof(node_t));//creation d un noeud
 			if(n==NULL){
 				perror("node creation");
 			}
 			n->candid=candidat;
 			list->first=n;
 			list->sizelist=1;
-			j++;
+			taille_fichier--;
 			pthread_mutex_unlock(&mut3);
 		}
 		if(test==max){
@@ -166,15 +162,15 @@ void *ecrire(){
 				n=n->next;
 			}
 			list->sizelist++;
-			j++;
+			taille_fichier--;
 			pthread_mutex_unlock(&mut3);
 		}
 	}
-	int fd2=open("File.txt", O_WRONLY|O_CREAT|O_TRUNC);//peut etre troisieme parametre qui correspond aux droits du fichier mais inutile je pense nom du fichier de sortie File mais jsp si nom demande
+	int fd2=open("File.txt", O_WRONLY|O_CREAT|O_TRUNC);//ouverture du fichier ou les candidats seront ecrits
 	if(fd2==-1){
 		perror("open File write");
 	}
-	for(int i=1;i<=list->sizelist;i++){
+	for(int i=1;i<=list->sizelist;i++){//parcourt la liste 
 		char *wbuff1=malloc(sizeof(char)*strlen(candidat)+1);
 		int b=write(fd2, wbuff1, sizeof(char)*(strlen(candidat)+1));
 		if(b==-1){
@@ -196,7 +192,7 @@ if(test==-1) //recuperer la taille du fichier
 	perror("stat");
 taille_fichier = recup->st_size;
 taille_fichier=taille_fichier/32; //divise par la taille d'un hash
-
+free(recup); // a voir normalement ok mais voir au cas ou beug
 //creation des sem
 	int e1=sem_init(&empty1, 0, 3);//cree semaphore vide qui compte 3 slots vides consommateur 1
 	if(e1==-1){//si erreur
@@ -214,6 +210,7 @@ taille_fichier=taille_fichier/32; //divise par la taille d'un hash
 	if(f2==-1){//si erreur
 		perror("create full2");
 	}
+
 //creation des mutex
 	int err=pthread_mutex_init(&mut1, NULL);
 	if (err!=0){ //si erreur quand on initialise mut1 
